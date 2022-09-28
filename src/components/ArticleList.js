15cee70b5818dce135cef4af59e264a8a1a1771d
moreenisaac <nkirotemoreen70@gmail.com>
2022-09-28 10:47:47 +0300

    done

---
+++ src/components/ArticleList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Article from './Article'
+
+function ArticleList({posts}) {
+  return (
+    <main>
+      {posts.map((posts) =>{
+        return(
+          <Article key={posts.id} title={posts.title} date={posts.date} preview={posts.preview} />
+        )
+      })}
+    </main>
+  )
+}
+
+export default ArticleList
